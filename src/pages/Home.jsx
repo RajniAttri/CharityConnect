ce83f483b4a8ab8e679e51447efe947bb40d840a
N-TITIAN <ndifon.titian@gmail.com>
2024-07-29 09:07:28 +0100

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -406,11 +406,54 @@ function Home(){
         </section>
 
         {/* review section  */}
-         <section className=" flex flex-row my-20 py-20 gap-8 items-center  bg-[#EEEAFC]">
-        <div>
-          0
-        </div>
-         </section>
+        <section className=" flex flex-row my-20 py-20 gap-8 items-center  bg-[#EEEAFC]">
+          <div className="flex flex-row gap-10">
+            <div className="w-1/3 px-20 text-center flex items-center bg-slate-600 bg-[ url('../../../public/images/Rectangle 11.png')]">
+              <p className=" text-3xl md:text-4xl font-bold ">
+                What People Say About Us
+              </p>
+            </div>
+            <div className="flex flex-col gap-8 pr-20">
+              <div>
+                <img src={image9} alt="" className=" h-18 w-18 " />
+                <p>
+                  It is long established fact that reader will distract by the
+                  readable content a page when looking atten layout. The point
+                  of using and that it has a normal distribution of letters
+                </p>
+              </div>
+              <div className="flex flex-row justify-between items-center">
+                <div className="flex flex-col gap-2">
+                  <p className="font-medium text-2xl">Active Member</p>
+                  <p className="">Volunteer</p>
+                </div>
+                <div className="flex flex-row p-4 items-start gap-4 text-2xl w-1/3  bg-primary-color text-white">
+                  <i class="fa fa-arrow-circle-o-left"></i>
+                  <i class="fa fa-arrow-circle-o-right"></i>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        {/* section news and blog  */}
+        <section className="mx-6 md:mx-32 flex flex-col items-center my-20 gap-10">
+          <p className=" text-3xl md:text-5xl font-bold text-center ">
+            Latest News From <br /> Givest Blog
+          </p>
+          <div className="flex flex-col gap-8 lg:flex-row">
+            
+            {/* card 2  */}
+
+            {/* card 3 */}
+          </div>
+
+          <div className="flex flex-row gap-1 ">
+            <div className="w-4 h-4 rounded-lg bg-[#9483F1]"></div>
+            <div className="w-16 h-4 rounded-lg bg-primary-color"></div>
+            <div className="w-4 h-4 rounded-lg bg-[#9483F1]"></div>
+          </div>
+        </section>
       </div>
     );
 }
