15df27c458cb830770fc7f4032e2751dac3ecaad
Spyke Lionel <social.lionel@gmail.com>
2024-08-05 10:52:31 +0100

    get cause details using id from useParams hook

--- src/pages/causes/CauseDetails.jsx
+++ src/pages/causes/CauseDetails.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import { useParams } from "react-router-dom";
 
 function CauseDetails() {
+  const id = useParams().id;
+  // Fetch cause data from an data store using the id parameter.
+
   return (
-    <div>CauseDetails</div>
-  )
+    <div className="flex justify-center text-5xl text-red-500">
+      Showing cause details for {id}
+    </div>
+  );
 }
 
-export default CauseDetails
+export default CauseDetails;
