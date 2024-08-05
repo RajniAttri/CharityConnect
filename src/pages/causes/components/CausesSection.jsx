d72c9828b8569047e963b68d98b9b8e401e06c4c
Spyke Lionel <social.lionel@gmail.com>
2024-08-05 10:51:18 +0100

    add data repository

--- src/pages/causes/components/CausesSection.jsx
+++ src/pages/causes/components/CausesSection.jsx
@@ -1,114 +1,114 @@
 import React from "react";
-import image from "../../../assets/images/Rectangle 5.png";
 import DonationCard from "../../home/components/DonationCard";
+import { donations } from "../data/data";
 
 function CausesSection() {
-  const donations = [
-    {
-      id: 1,
-      title: "Children Education Needs For Change The World.",
-      description:
-        "Lorem Ipsum is simply dummy text of the industry's since the unknown",
-      image,
-      author: "Adam",
-      items: [
-        { name: "Goal", amount: "$40000" },
-        { name: "Rice", amount: "$30000" },
-        { name: "To Go", amount: "$40000" },
-      ],
-    },
-    {
-      id: 2,
-      title: "Children Education Needs For Change The World.",
-      description:
-        "Lorem Ipsum is simply dummy text of the industry's since the unknown",
-      image: image,
-      author: "Adam",
-      items: [
-        { name: "Goal", amount: "$40000" },
-        { name: "Rice", amount: "$30000" },
-        { name: "To Go", amount: "$40000" },
-      ],
-    },
-    {
-      id: 3,
-      title: "Children Education Needs For Change The World.",
-      description:
-        "Lorem Ipsum is simply dummy text of the industry's since the unknown",
-      image: image,
-      author: "Adam",
-      items: [
-        { name: "Goal", amount: "$40000" },
-        { name: "Rice", amount: "$30000" },
-        { name: "To Go", amount: "$40000" },
-      ],
-    },
-    {
-      id: 4,
-      title: "Children Education Needs For Change The World.",
-      description:
-        "Lorem Ipsum is simply dummy text of the industry's since the unknown",
-      image: image,
-      author: "Adam",
-      items: [
-        { name: "Goal", amount: "$40000" },
-        { name: "Rice", amount: "$30000" },
-        { name: "To Go", amount: "$40000" },
-      ],
-    },
-    {
-      id: 5,
-      title: "Children Education Needs For Change The World.",
-      description:
-        "Lorem Ipsum is simply dummy text of the industry's since the unknown",
-      image: image,
-      author: "Adam",
-      items: [
-        { name: "Goal", amount: "$40000" },
-        { name: "Rice", amount: "$30000" },
-        { name: "To Go", amount: "$40000" },
-      ],
-    },
-    {
-      id: 6,
-      title: "Children Education Needs For Change The World.",
-      description:
-        "Lorem Ipsum is simply dummy text of the industry's since the unknown",
-      image: image,
-      author: "Adam",
-      items: [
-        { name: "Goal", amount: "$40000" },
-        { name: "Rice", amount: "$30000" },
-        { name: "To Go", amount: "$40000" },
-      ],
-    },
-    {
-      id: 7,
-      title: "Children Education Needs For Change The World.",
-      description:
-        "Lorem Ipsum is simply dummy text of the industry's since the unknown",
-      image: image,
-      author: "Adam",
-      items: [
-        { name: "Goal", amount: "$40000" },
-        { name: "Rice", amount: "$30000" },
-        { name: "To Go", amount: "$40000" },
-      ],
-    },
-    {
-      id: 8,
-      title: "Children Education Needs For Change The World.",
-      description:
-        "Lorem Ipsum is simply dummy text of the industry's since the unknown",
-      image: image,
-      author: "Adam",
-      items: [
-        { name: "Goal", amount: "$40000" },
-        { name: "Rice", amount: "$30000" },
-        { name: "To Go", amount: "$40000" },
-      ],
-    },
-  ];
+  // const donations = [
+  //   {
+  //     id: 1,
+  //     title: "Children Education Needs For Change The World.",
+  //     description:
+  //       "Lorem Ipsum is simply dummy text of the industry's since the unknown",
+  //     image,
+  //     author: "Adam",
+  //     items: [
+  //       { name: "Goal", amount: "$40000" },
+  //       { name: "Rice", amount: "$30000" },
+  //       { name: "To Go", amount: "$40000" },
+  //     ],
+  //   },
+  //   {
+  //     id: 2,
+  //     title: "Children Education Needs For Change The World.",
+  //     description:
+  //       "Lorem Ipsum is simply dummy text of the industry's since the unknown",
+  //     image: image,
+  //     author: "Adam",
+  //     items: [
+  //       { name: "Goal", amount: "$40000" },
+  //       { name: "Rice", amount: "$30000" },
+  //       { name: "To Go", amount: "$40000" },
+  //     ],
+  //   },
+  //   {
+  //     id: 3,
+  //     title: "Children Education Needs For Change The World.",
+  //     description:
+  //       "Lorem Ipsum is simply dummy text of the industry's since the unknown",
+  //     image: image,
+  //     author: "Adam",
+  //     items: [
+  //       { name: "Goal", amount: "$40000" },
+  //       { name: "Rice", amount: "$30000" },
+  //       { name: "To Go", amount: "$40000" },
+  //     ],
+  //   },
+  //   {
+  //     id: 4,
+  //     title: "Children Education Needs For Change The World.",
+  //     description:
+  //       "Lorem Ipsum is simply dummy text of the industry's since the unknown",
+  //     image: image,
+  //     author: "Adam",
+  //     items: [
+  //       { name: "Goal", amount: "$40000" },
+  //       { name: "Rice", amount: "$30000" },
+  //       { name: "To Go", amount: "$40000" },
+  //     ],
+  //   },
+  //   {
+  //     id: 5,
+  //     title: "Children Education Needs For Change The World.",
+  //     description:
+  //       "Lorem Ipsum is simply dummy text of the industry's since the unknown",
+  //     image: image,
+  //     author: "Adam",
+  //     items: [
+  //       { name: "Goal", amount: "$40000" },
+  //       { name: "Rice", amount: "$30000" },
+  //       { name: "To Go", amount: "$40000" },
+  //     ],
+  //   },
+  //   {
+  //     id: 6,
+  //     title: "Children Education Needs For Change The World.",
+  //     description:
+  //       "Lorem Ipsum is simply dummy text of the industry's since the unknown",
+  //     image: image,
+  //     author: "Adam",
+  //     items: [
+  //       { name: "Goal", amount: "$40000" },
+  //       { name: "Rice", amount: "$30000" },
+  //       { name: "To Go", amount: "$40000" },
+  //     ],
+  //   },
+  //   {
+  //     id: 7,
+  //     title: "Children Education Needs For Change The World.",
+  //     description:
+  //       "Lorem Ipsum is simply dummy text of the industry's since the unknown",
+  //     image: image,
+  //     author: "Adam",
+  //     items: [
+  //       { name: "Goal", amount: "$40000" },
+  //       { name: "Rice", amount: "$30000" },
+  //       { name: "To Go", amount: "$40000" },
+  //     ],
+  //   },
+  //   {
+  //     id: 8,
+  //     title: "Children Education Needs For Change The World.",
+  //     description:
+  //       "Lorem Ipsum is simply dummy text of the industry's since the unknown",
+  //     image: image,
+  //     author: "Adam",
+  //     items: [
+  //       { name: "Goal", amount: "$40000" },
+  //       { name: "Rice", amount: "$30000" },
+  //       { name: "To Go", amount: "$40000" },
+  //     ],
+  //   },
+  // ];
 
   return (
     <section className="mx-6 md:mx-32 flex flex-col items-center my-20 gap-10">
