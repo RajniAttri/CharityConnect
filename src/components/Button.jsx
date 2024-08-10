5f73b3e9d636a65c9708c82e819b5c23ae9c288d
Spyke Lionel <social.lionel@gmail.com>
2024-08-10 11:09:50 +0100

    refactor: add extra props to button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -6,23 +6,18 @@ import { Link } from "react-router-dom";
  * @param {*} props The props
  * @returns
  */
-function Button(props) {
-  const icon = props.icon;
-  const text = props.text;
-  const type = props.type;
-  const href = props.href;
-
+function Button({ icon, text, type, href, className, ...props }) {
   /**
    * type = 'primary-button' | 'secondary-button'
    */
 
   return type == "primary-link" || type == "secondary-link" ? (
-    <Link className={type} to={href}>
+    <Link className={`${type} ${className}`} to={href} {...props}>
       {text}
       {icon}
     </Link>
   ) : (
-    <button className={type}>
+    <button className={`${type} ${className}`} {...props}>
       {text}
       {icon}
     </button>
