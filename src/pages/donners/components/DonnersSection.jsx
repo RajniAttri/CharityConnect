80b7adb40a966582d11e293ccd42f066c546fbe4
Spyke Lionel <59184392+spykelionel@users.noreply.github.com>
2024-08-11 10:56:57 +0100

    Update DonnersSection.jsx
    Signed-off-by: Spyke Lionel <59184392+spykelionel@users.noreply.github.com>

--- src/pages/donners/components/DonnersSection.jsx
+++ src/pages/donners/components/DonnersSection.jsx
@@ -6,9 +6,7 @@ import image20 from '../../../assets/images/Rectangle20.jpg'
 import image14 from '../../../assets/images/Ellipse 94.png'
 import image10 from "../../../assets/images/man.jpg";
 import image12 from "../../../assets/images/Rayon.jpg";
-
 import image13 from "../../../assets/images/muslim.jpg";
-
 import image21 from "../../../assets/images/Rectangle21.jpg"
 import image22 from "../../../assets/images/Rectangle22.jpg";
 import image23 from "../../../assets/images/Rectangle23.jpg";
@@ -20,9 +18,6 @@ const DonnersSection = () => {
         <h1 className='text-4xl pb-7 font-semibold text-center'>Meet Our Donners</h1>
         <p className=' text-center pb-10 text-2xl px-28 pr-28'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem suscipit dolor architecto. 
             Repudiandae quae aut beatae tempora. Voluptatibus, doloremque ullam!</p>
-
-
-
             <div className='flex g-3 justify-evenly '>
             <div>
             <img src={image19}  alt='' className='w-80 h-96'/>
@@ -76,8 +71,6 @@ const DonnersSection = () => {
 
           </div>
 
-           
-      
     </div>
       
     </div>
